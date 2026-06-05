4f18ad24ebf69ecd259c98ecfb97f31f5e3ddf43
alaminjava <alaminhabibulgmail.com>
2026-06-05 05:34:44 -0400

    Handle API preflight CORS requests

--- server/index.js
+++ server/index.js
@@ -30,6 +30,19 @@ const isAllowedOrigin = (origin) => {
     return false;
   }
 };
+app.use((req, res, next) => {
+  const origin = req.headers.origin;
+
+  if (isAllowedOrigin(origin)) {
+    res.header('Access-Control-Allow-Origin', origin || '*');
+    res.header('Access-Control-Allow-Credentials', 'true');
+    res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization');
+  }
+
+  if (req.method === 'OPTIONS') return res.sendStatus(204);
+  return next();
+});
 const corsOptions = {
   origin: (origin, cb) => cb(null, isAllowedOrigin(origin)),
   credentials: true,
